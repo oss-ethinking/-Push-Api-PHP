3fe1be3d002c1c9b39c5396b63f1706c05503874
Oleh Pylypchuk <opy@ethinking.de>
2020-05-20 13:30:22 +0200

    Add twig extension

---
+++ src/bundle/DependencyInjection/EthinkingPushApiExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ethinking\EthinkingPushApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
+
+/**^
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ */
+class EthinkingPushApiExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $fileLocator = new FileLocator(__DIR__ . '/../Resources/config');
+
+        $resolver = new LoaderResolver([
+            new Loader\YamlFileLoader($container, $fileLocator),
+            new Loader\DirectoryLoader($container, $fileLocator),
+        ]);
+
+        $loader = new DelegatingLoader($resolver);
+        $loader->load('services.yaml');
+        $loader->load('extensions.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+    }
+}
---
+++ src/bundle/Templating/Twig/Extension/EmbedCodeExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Ethinking\EthinkingPushApiBundle\Templating\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Twig\Environment;
+
+class EmbedCodeExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction(
+                'ethinking_push_embed_code',
+                [$this, 'generate'],
+                [
+                    'is_safe' => ['html'],
+                    'needs_environment' => true
+                ]
+            )];
+    }
+
+    public function generate(Environment $environment, $config): string
+    {
+        if (empty($config)) {
+            return "";
+        }
+
+        $configJson = json_encode($config);
+
+        if ($configJson === false) {
+            return "";
+        }
+
+        return <<<EOD
+<script src="<path_to_library>/webpush-development.js"></script>
+<script type="text/javascript">
+    window.addEventListener('load', () => {
+        var config = $configJson;
+
+        var push = new PushDelivery(config);
+
+        if (push.isSupportedBrowser() && push.requestPermission()) {
+            push.init();
+        }
+    });
+</script>
+EOD;
+    }
+}
