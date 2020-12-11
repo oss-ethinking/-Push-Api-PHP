37937b19c2762e937e0aee960f3714df61961d78
Oleh Pylypchuk <opy@ethinking.de>
2020-12-11 12:45:28 +0100

    Add more tests for PushApiInstance class

--- tests/PushApiInstanceTest.php
+++ tests/PushApiInstanceTest.php
@@ -3,8 +3,13 @@
 namespace Ethinking\Test;
 
 use Ethinking\EthinkingPushApiBundle\Entity\Channel;
+use Ethinking\EthinkingPushApiBundle\Entity\History;
+use Ethinking\EthinkingPushApiBundle\Entity\HistoryDetails;
+use Ethinking\EthinkingPushApiBundle\Service\FormDataPart;
+use Ethinking\EthinkingPushApiBundle\Service\JsonPart;
 use Ethinking\EthinkingPushApiBundle\Service\PushApiInstance;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 
@@ -152,6 +157,474 @@ class PushApiInstanceTest extends TestCase
         $this->assertTrue($clearCacheResult);
     }
 
+    public function testAddChannelAndDefaultTag_returnsTrue(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $addTagUri = "/push-admin-api/tag/add/{$clientId}";
+        $addChannelUri = "/push-admin-api/app/add/{$clientId}";
+        $defaultTagSourceId = "push-connector-tag-general-{$clientId}";
+        $addTagResponse = $this->getResponse();
+        $addChannelResponse = $this->getResponse();
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->at(0))
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $addTagUri))
+            ->willReturn($addTagResponse);
+
+        $this->httpClient->expects($this->at(1))
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $addChannelUri))
+            ->willReturn($addChannelResponse);
+
+        // When
+        $result = $this->pushApiService->addChannelAndDefaultTag(new Channel());
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function testAddChannel_returnsTrue(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $uri = "/push-admin-api/app/add/{$clientId}";
+        $response = $this->getResponse();
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $uri))
+            ->willReturn($response);
+
+        // When
+        $result = $this->pushApiService->addChannel(new Channel());
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function testAddChannel_returnsFalse(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $uri = "/push-admin-api/app/add/{$clientId}";
+        $response = $this->getResponse('{}', 500);
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $uri))
+            ->willReturn($response);
+
+        // When
+        $result = $this->pushApiService->addChannel(new Channel());
+
+        // Then
+        $this->assertFalse($result);
+    }
+
+    public function testUpdateChannel_returnsTrue(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $uri = "/push-admin-api/app/update/{$clientId}";
+        $response = $this->getResponse();
+        $channel = new Channel();
+        $channel->setId('1');
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $uri))
+            ->willReturn($response);
+
+        // When
+        $result = $this->pushApiService->updateChannel($channel);
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function testUpdateChannel_returnsFalse(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $uri = "/push-admin-api/app/update/{$clientId}";
+        $response = $this->getResponse('{}', 500);
+        $channel = new Channel();
+        $channel->setId('1');
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $uri))
+            ->willReturn($response);
+
+        // When
+        $result = $this->pushApiService->updateChannel($channel);
+
+        // Then
+        $this->assertFalse($result);
+    }
+
+    public function testUpdateChannel_throwsBadRequestHttpException(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $uri = "/push-admin-api/app/update/{$clientId}";
+        $response = $this->getResponse();
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->expectException(BadRequestHttpException::class);
+
+        // When
+        $this->pushApiService->updateChannel(new Channel());
+    }
+
+    public function testDeleteChannel_returnsTrue(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $uri = "/push-admin-api/app/delete/{$clientId}";
+        $response = $this->getResponse();
+        $id = 'id';
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $uri))
+            ->willReturn($response);
+
+        // When
+        $result = $this->pushApiService->deleteChannel($id);
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function testGetHistory_returnsHistory(): void
+    {
+        // Given
+        $page = 2;
+        $count = 1;
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $username = 'random-username';
+        $password = 'random-password';
+        $uri = "/push-admin-api/history/get/{$clientId}?page={$page}&count={$count}";
+        $historyJson = '[{"id":"1","userName":"2","status":"3","time":"4"}]';
+        $response = $this->getResponse($historyJson);
+        $expectedHistory = new History('1', '2', '3', '4');
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->settings->expects($this->any())
+            ->method('getUsername')
+            ->willReturn($username);
+
+        $this->settings->expects($this->any())
+            ->method('getPassword')
+            ->willReturn($password);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('GET'), $this->equalTo($domain . $uri), $this->equalTo([
+                'auth_basic' => "{$username}:{$password}"
+            ]))
+            ->willReturn($response);
+
+        // When
+        $historyArray = $this->pushApiService->getHistory($page, $count);
+
+        // Then
+        $this->assertEquals($expectedHistory, current($historyArray));
+    }
+
+    public function testGetHistoryDetails_returnsHistoryDetails(): void
+    {
+        // Given
+        $id = 1;
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $username = 'random-username';
+        $password = 'random-password';
+        $uri = "/push-api/status/{$id}";
+        $historyJson = '[{"key":{"id":"1","platformId":"2"},"state":"3","totalCount":"4","successCount":"5"}]';
+        $response = $this->getResponse($historyJson);
+        $expectedHistoryDetails = new HistoryDetails('1', '2', '3', '4', '5');
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->settings->expects($this->any())
+            ->method('getUsername')
+            ->willReturn($username);
+
+        $this->settings->expects($this->any())
+            ->method('getPassword')
+            ->willReturn($password);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('GET'), $this->equalTo($domain . $uri), $this->equalTo([
+                'auth_basic' => "{$username}:{$password}"
+            ]))
+            ->willReturn($response);
+
+        // When
+        $historyDetailsArray = $this->pushApiService->getHistoryDetails($id);
+
+        // Then
+        $this->assertEquals($expectedHistoryDetails, current($historyDetailsArray));
+    }
+
+    public function testAddTag_returnsTrue(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $addTagUri = "/push-admin-api/tag/add/{$clientId}";
+        $addTagResponse = $this->getResponse();
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $addTagUri))
+            ->willReturn($addTagResponse);
+
+        // When
+        $result = $this->pushApiService->addTag("random", "random");
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function testSendPushNotification_returnsTrue(): void
+    {
+        // Given
+        $domain = 'random-domain';
+        $uri = "/push-api/push";
+        $response = $this->getResponse();
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $uri))
+            ->willReturn($response);
+
+        // When
+        $result = $this->pushApiService->sendPushNotification([]);
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function testSendPush_returnsTrue(): void
+    {
+        // Given
+        $domain = 'random-domain';
+        $uri = "/push-api/push";
+        $response = $this->getResponse();
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('POST'), $this->equalTo($domain . $uri))
+            ->willReturn($response);
+
+        // When
+        $result = $this->pushApiService->sendPush([]);
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function testGetDefaultTagSourceId_returnsDefaultTagId(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $expectedDefaultTagSourceId = "push-connector-tag-general-{$clientId}";
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        // When
+        $defaultTagSourceId = $this->pushApiService->getDefaultTagSourceId();
+
+        // Then
+        $this->assertEquals($expectedDefaultTagSourceId, $defaultTagSourceId);
+    }
+
+    public function testGetDefaultTagId_returnsDefaultTagId(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $username = 'random-username';
+        $password = 'random-password';
+        $field = 'sourceId';
+        $expectedId = "push-connector-tag-general-{$clientId}";
+        $uri = "/push-admin-api/tag/{$field}/{$expectedId}/{$clientId}";
+        $tagJson = '{"id":"' . $expectedId . '"}';
+        $response = $this->getResponse($tagJson);
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->settings->expects($this->any())
+            ->method('getUsername')
+            ->willReturn($username);
+
+        $this->settings->expects($this->any())
+            ->method('getPassword')
+            ->willReturn($password);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('GET'), $this->equalTo($domain . $uri), $this->equalTo([
+                'auth_basic' => "{$username}:{$password}"
+            ]))
+            ->willReturn($response);
+
+        // When
+        $defaultTagId = $this->pushApiService->getDefaultTagId();
+
+        // Then
+        $this->assertEquals($expectedId, $defaultTagId);
+    }
+
+    public function testGetTag_returnsTag(): void
+    {
+        // Given
+        $clientId = 'random-string';
+        $domain = 'random-domain';
+        $username = 'random-username';
+        $password = 'random-password';
+        $field = 'random-field';
+        $id = '1';
+        $uri = "/push-admin-api/tag/{$field}/{$id}/{$clientId}";
+        $tagJson = '{"id":"1"}';
+        $response = $this->getResponse($tagJson);
+        $expectedTag = ['id' => '1'];
+
+        $this->settings->expects($this->any())
+            ->method('getClientId')
+            ->willReturn($clientId);
+
+        $this->settings->expects($this->any())
+            ->method('getDomain')
+            ->willReturn($domain);
+
+        $this->settings->expects($this->any())
+            ->method('getUsername')
+            ->willReturn($username);
+
+        $this->settings->expects($this->any())
+            ->method('getPassword')
+            ->willReturn($password);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo('GET'), $this->equalTo($domain . $uri), $this->equalTo([
+                'auth_basic' => "{$username}:{$password}"
+            ]))
+            ->willReturn($response);
+
+        // When
+        $tag = $this->pushApiService->getTag($field, $id);
+
+        // Then
+        $this->assertEquals($expectedTag, $tag);
+    }
+
     private function expectChannels()
     {
         $webPushPlatformId = '7';
@@ -200,16 +673,24 @@ class PushApiInstanceTest extends TestCase
             ->willReturn($defaultTagResponse);
     }
 
-    private function getResponse(string $content): ResponseInterface
+    private function getResponse(string $content = '{}', int $statusCode = 200): ResponseInterface
     {
         $response = new class implements ResponseInterface {
             private $content;
+            private $statusCode;
 
-            public function getStatusCode(): int {}
             public function getHeaders(bool $throw = true): array {}
             public function toArray(bool $throw = true): array {}
             public function cancel(): void {}
             public function getInfo(string $type = null) {}
+
+            public function getStatusCode(): int {
+                return $this->statusCode;
+            }
+
+            public function setStatusCode(int $statusCode) {
+                $this->statusCode = $statusCode;
+            }
 
             public function getContent(bool $throw = true): string
             {
@@ -222,6 +703,7 @@ class PushApiInstanceTest extends TestCase
             }
         };
 
+        $response->setStatusCode($statusCode);
         $response->setContent($content);
 
         return $response;
